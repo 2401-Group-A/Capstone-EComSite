1495794322099a611c6f3549386eb718622dbc10
Brittany Dugger <brittanydugger@Brittanys-MacBook-Pro.local>
2024-03-15 13:49:24 -0500

    added basic css to navbar

--- src/client/components/NavBar.jsx
+++ src/client/components/NavBar.jsx
@@ -1,18 +1,21 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
+import './styles/navbar.css'
 // import logo??
 
-
 export default function NavBar() {
-    return (
-        <header>
-            <section>
-        <h1>LOGO</h1>
-            </section>
-        <nav>
-            <Link to='/seeds'>Home</Link>
-            <Link to='/account'>Account</Link>
-            <Link to='/cart'>Cart</Link>
-        </nav>
-        </header>
-)}
+  
+  return (
+    <header className="navbar-container">
+      <section>
+        <h1 className="nav-logo">LOGO</h1>
+      </section>
+
+      <nav>
+        <Link to="/seeds">Home</Link>
+        <Link to="/account">Account</Link>
+        <Link to="/cart">Cart</Link>
+      </nav>
+    </header>
+  );
+}
