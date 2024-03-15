c4bfc4c2c66e8ae2f4830b2901118c74d68af081
Brittany Dugger <brittanydugger@Brittanys-MacBook-Pro.local>
2024-03-15 09:39:33 -0500

    updated App.jsx to have routes and token useState

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -2,12 +2,21 @@ import { useState } from "react";
 import Login from "./components/Login";
 
 function App() {
+  const [toke, useToken] = useState(null)
   return (
     <>
       <NavBar/>
       <Routes>
-
+        <Route path='./components/admin/Inventory'></Route>
+        <Route path='./components/admin/UserData'></Route>
+        <Route path='./components/admin/UserOrders'></Route>
+        <Route path='./components/Account'></Route>
+        <Route path='./components/Cart'></Route>
+        <Route path='./components/Checkout'></Route>
+        <Route path='./components/Login'></Route>
+        <Route path='./components/Register'></Route>
       </Routes>
+      
       <div className="App">
         <h1>Boilerplate</h1>
         <img id="comp-img" src="./computer.png"></img>
