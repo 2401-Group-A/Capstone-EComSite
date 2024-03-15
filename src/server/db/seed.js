39329aec3594cdbc52b6e0f7d7279ecbdaa37abb
Josh <joshuamace@gmail.com>
2024-03-15 13:05:46 -0500

    commented out unused code

--- src/server/db/seed.js
+++ src/server/db/seed.js
@@ -1,34 +1,34 @@
 const db = require('./client');
 const { createUser } = require('./users');
 
-const users = [
-  {
-    name: 'Emily Johnson',
-    email: 'emily@example.com',
-    password: 'securepass',
-  },
-  {
-    name: 'Liu Wei',
-    email: 'liu@example.com',
-    password: 'strongpass',
-  },
-  {
-    name: 'Isabella García',
-    email: 'bella@example.com',
-    password: 'pass1234',
-  },
-  {
-    name: 'Mohammed Ahmed',
-    email: 'mohammed@example.com',
-    password: 'mysecretpassword',
-  },
-  {
-    name: 'John Smith',
-    email: 'john@example.com',
-    password: 'password123',
-  },
-  // Add more user objects as needed
-];
+// const users = [
+//   {
+//     name: 'Emily Johnson',
+//     email: 'emily@example.com',
+//     password: 'securepass',
+//   },
+//   {
+//     name: 'Liu Wei',
+//     email: 'liu@example.com',
+//     password: 'strongpass',
+//   },
+//   {
+//     name: 'Isabella García',
+//     email: 'bella@example.com',
+//     password: 'pass1234',
+//   },
+//   {
+//     name: 'Mohammed Ahmed',
+//     email: 'mohammed@example.com',
+//     password: 'mysecretpassword',
+//   },
+//   {
+//     name: 'John Smith',
+//     email: 'john@example.com',
+//     password: 'password123',
+//   },
+//   // Add more user objects as needed
+// ];
 
 const dropTables = async () => {
   try {
