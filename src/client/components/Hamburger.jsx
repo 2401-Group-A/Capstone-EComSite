9d3143dec46fcb4606bd7ea994e31b501218ba4f
Brittany Dugger <brittanydugger@Brittanys-MacBook-Pro.local>
2024-03-17 17:05:37 -0500

    working on menu button

---
+++ src/client/components/Hamburger.jsx
@@ -0,0 +1,11 @@
+export default function Hamburger() {
+  return (
+    <>
+      <div className="hamburger">
+        <div className="burger burger1"></div>
+        <div className="burger burger2"></div>
+        <div className="burger burger3"></div>
+      </div>
+    </>
+  );
+}
--- src/client/components/NavBar.jsx
+++ src/client/components/NavBar.jsx
@@ -1,14 +1,20 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import './styles/navbar.css'
+import Hamburger from "./Hamburger";
+import {useState} from 'react'
 // import logo??
 
 export default function NavBar() {
-  
+  // const [hamburgerOpen, setHamburgerOpen] = useState(false)
+  // const toggleHamburger = () => {
+  //   setHamburgerOpen(!hamburgerOpen)
+  // }
   return (
     <header className="navbar-container">
       <section>
-        <h1 className="nav-logo">LOGO</h1>
+        <Hamburger/>
+         <h1 className="nav-logo">LOGO</h1>
       </section>
 
       <nav>
