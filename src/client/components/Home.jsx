f0d8dea712ac8bd27d8797a1bd3dda292ce21ce1
Michael  Jaroszynski <mikeyj@Mikey-Js-MacBook-Pro.local>
2024-03-18 11:01:15 -0400

    moved all file specific CSS to /Styles folder

--- src/client/components/Home.jsx
+++ src/client/components/Home.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import './Home.css'; 
+import './styles/Home.css'; 
 
 function Home() {
   return (
--- src/client/components/Register.jsx
+++ src/client/components/Register.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import './register.css';
+import './styles/register.css';
 
 const Register = () => {
     const [email, setEmail] = useState('');
