901dd069ccadaf14af26b3332f3204454a1c14c4
Josh <joshuamace@gmail.com>
2024-03-19 14:14:37 -0500

    added navigation for account and login

--- src/client/components/Account.jsx
+++ src/client/components/Account.jsx
@@ -1,47 +1,57 @@
-import { useState, useEffect } from "react"
-import axios from "axios"
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
-export default function Account ({ token }) {
-    const [account, setAccount] = useState(null);
-    const [orders, setOrders] = useState([]);
-    
-    
-    useEffect(() => {
-        async function loadAccount() {
-            try {
-                const accountResponse = await axios.get('', {
-                    headers: {
-                        Authorization: 'Bearer ' + token
-                    }
-                })
-                // accountResponse looks like { data: actual data}
-                setAccount(accountResponse.data) // instead of `const { data } =`
+export default function Account({ token }) {
+  const [profile, setProfile] = useState({});
+  const [orders, setOrders] = useState([]);
+  const navigate = useNavigate();
 
-                const ordersResponse = await axios.get('', {
-                    headers: {
-                        Authorization: 'Bearer ' + token
-                    }
-                })
-                // ordersResponse looks like { data: actual data }
-                console.log('ordersResponse', ordersResponse)
-                setOrders(ordersResponse.data.orders)
-            } catch (err) {
-                console.error(err)
-            }
+  useEffect(() => {
+    async function loadProfile() {
+      try {
+        if (!token) {
+          setTimeout(function () {
+            navigate('/login');
+          }, 3000);
+        } else {
+          const accountResponse = await axios.get('', {
+            headers: {
+              Authorization: 'Bearer ' + token,
+            },
+          });
+          // accountResponse looks like { data: actual data}
+          setProfile(accountResponse.data); // instead of `const { data } =`
+          console.log(profile);
+          const ordersResponse = await axios.get('', {
+            headers: {
+              Authorization: 'Bearer ' + token,
+            },
+          });
+          // ordersResponse looks like { data: actual data }
+          console.log('ordersResponse', ordersResponse);
+          setOrders(ordersResponse.data.orders);
         }
-        loadAccount()
-    }, [token])
-
-    if (!account) {
-        return <div>Not logged in</div>
+      } catch (err) {
+        console.error(err);
+      }
     }
+    loadProfile();
+  }, []);
 
-    return <main>
-        <h1>{account.firstname} {account.lastname}</h1>
-        <h2>{account.email}</h2>
-        <div>Orders Made:
-        </div>
-        {
+  if (!token) {
+    return <div>You are not logged in. Please log in to view your account</div>;
+  }
+
+  return (
+    <main>
+      <h1>
+        {profile.firstname} {profile.lastname}
+      </h1>
+      <h2>{profile.email}</h2>
+      <div>
+        <p>Orders Made:</p>
+        {/* {
             orders.map(order => {
                 return (
                     <article key={order.id}>
@@ -50,9 +60,8 @@ export default function Account ({ token }) {
                     </article>
                 )
             })
-        }
-    
+        } */}
+      </div>
     </main>
-
+  );
 }
-
