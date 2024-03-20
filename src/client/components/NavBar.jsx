e6f13dc67da61f838e9d396b896fae3ded789e88
Michael  Jaroszynski <mikeyj@Mikey-Js-MacBook-Pro.local>
2024-03-20 10:20:48 -0400

    Changed router of home from /seeds to / in app/navbar.jsx

--- src/client/components/NavBar.jsx
+++ src/client/components/NavBar.jsx
@@ -1,21 +1,20 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import './styles/navbar.css'
+import "./styles/navbar.css";
 import Hamburger from "./Hamburger";
-import {useState} from 'react'
+import { useState } from "react";
 // import logo??
 
 export default function NavBar() {
-  
   return (
     <header className="navbar-container">
       <section className="burger-container">
-        <Hamburger/>
+        <Hamburger />
       </section>
 
       <h1 className="nav-logo">LOGO</h1>
       <nav>
-        <Link to="/seeds">Home</Link>
+        <Link to="/">Home</Link>
         {/* <Link to="/account">Account</Link> */}
         <Link to="/cart">Cart</Link>
       </nav>
