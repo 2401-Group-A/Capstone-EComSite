bb3eaf0208a7d0ed9988919692db1f7f6e6f007b
Brittany Dugger <brittanydugger@Brittanys-MacBook-Pro.local>
2024-03-21 11:56:31 -0500

    added some cart css and started total price function

--- src/client/components/Cart.jsx
+++ src/client/components/Cart.jsx
@@ -1,21 +1,38 @@
 import {useState} from 'react'
-
+import './styles/cart.css'
 
 
 export default function Cart({cart, setCart}) {
     const [price, setPrice] = useState(0);
+
+    const handlePrice = () => {
+        let cartTotal = 0;
+        cart.map((product) => (
+            cartTotal += product.amount * product.price
+        ))
+        setPrice(cartTotal);
+    }
+
+useEffect(() => {
+    handlePrice()
+})
+
+
+
+
   return (
-    <>
+    <article>
       {
         cart?.map((product) => (
             <div className="cart_box" key={product.id}> 
                 <div className="cart_img">
                     <img src={product.imgurl} />
                     <h1 className="plant-type">{product.planttype}</h1>
-                    <h1 className="plant-variety">{product.plantvariety}</h1>
+                
                 </div>
-                <div>
+                <div className='button-box'>
                     <button> + </button>
+                    <button>{product.amount}</button>
                     <button> - </button>
                 </div>
                 <div>
@@ -25,10 +42,10 @@ export default function Cart({cart, setCart}) {
             
             </div>
         ))}
-        <div>
+        <div className='total'>
             <span> Total Price: </span>
             <span> ${price}</span>
         </div>
-    </>
+    </article>
   );
 }
