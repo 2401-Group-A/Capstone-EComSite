e711c3c85e900f8f22fc9d32f5c0d28e7e3911cd
Josh <joshuamace@gmail.com>
2024-03-26 14:57:29 -0500

    created /removeitem in api/cart.js

--- src/server/api/cart.js
+++ src/server/api/cart.js
@@ -1,6 +1,6 @@
-const express = require("express");
+const express = require('express');
 const cartRouter = express.Router();
-const requireToken = require("./requireToken");
+const requireToken = require('./requireToken');
 const {
   getCartItems,
   createCart,
@@ -8,96 +8,96 @@ const {
   getOrderItems,
   getCart,
   getPastOrders,
-  addToCart
-} = require('../db/cart')
-
+  addToCart,
+  updateCart,
+  deleteCartItem,
+} = require('../db/cart');
 
 // getting cart api endpoint
 cartRouter.get('/', requireToken, async (req, res, next) => {
-  const user_id = req.user.id
-  try{
-   const cart = await getCart(user_id);
-   res.json(cart);
-  }catch (err){
-    next(err)
+  const user_id = req.user.id;
+  try {
+    const cart = await getCart(user_id);
+    res.json(cart);
+  } catch (err) {
+    next(err);
   }
-})
+});
 
-// getting cart items 
+// getting cart items
 cartRouter.get('/cartId/:cartId', requireToken, async (req, res, next) => {
   const user_id = req.user.id;
-  console.log(user_id)
+  console.log(user_id);
 
-  try{
+  try {
     const cartId = req.params.cartId;
     const produce = await getCartItems(cartId);
     res.json(produce);
-  }catch (err){
-    next (err);
+  } catch (err) {
+    next(err);
   }
-})
+});
 
-
-// getting past orders 
+// getting past orders
 cartRouter.get('/orders', requireToken, async (req, res, next) => {
-  const user_id = req.user.id
-  console.log('user Id', req.user.id)
-  try{
-   const pastOrders = await getPastOrders(user_id);
-   res.json(pastOrders);
-  }catch (err){
-    next(err); 
+  const user_id = req.user.id;
+  console.log('user Id', req.user.id);
+  try {
+    const pastOrders = await getPastOrders(user_id);
+    res.json(pastOrders);
+  } catch (err) {
+    next(err);
   }
-} )
+});
 
-// add item to cart 
+// add item to cart
 
 cartRouter.post('/addproduct', async (req, res, next) => {
-  try{
-    const { order_id, product_id, quantity} = req.body
-    console.log('this is req.body',req.body)
-    await addToCart(order_id, product_id, quantity)
-    
-    res.status(201).send('This item has been added successfully')
-  }catch (err){
-    next(err)
+  try {
+    const { order_id, product_id, quantity } = req.body;
+    console.log('this is req.body', req.body);
+    await addToCart(order_id, product_id, quantity);
+
+    res.status(201).send('This item has been added successfully');
+  } catch (err) {
+    next(err);
   }
-})
+});
 
 // Change an items qty in the cart
 cartRouter.patch('/editcart', async (req, res, next) => {
-try {
-  // We need three pieces of info, the cart #, prduct # & qty
-  const { order_id, product_id, quantity} = req.body
-  await updateCart (order_id, product_id, quantity)
-  res.status(201).send('The quantity has been updated for this item.')
-} catch (err) {
-  next(err)
-}
-})
+  try {
+    // We need three pieces of info, the cart #, prduct # & qty
+    const { order_id, product_id, quantity } = req.body;
+    await updateCart(order_id, product_id, quantity);
+    res.status(201).send('The quantity has been updated for this item.');
+  } catch (err) {
+    next(err);
+  }
+});
 
+// Remove an item from your cart
 
-
-
-
-
-
-
+cartRouter.delete('/removeitem', async (req, res, next) => {
+  try {
+    const { order_id, product_id } = req.body;
+    await deleteCartItem(order_id, product_id);
+    res.status(201).send('This item has been removed from the cart.');
+  } catch (err) {
+    next(err);
+  }
+});
 
 // create order endpoint
 cartRouter.post('/', async (req, res, next) => {
   const { user_id } = req.body;
-console.log('recieved user_id', user_id)
-  try{
+  console.log('recieved user_id', user_id);
+  try {
     const order = await createOrder(user_id);
     res.status(201).json(order);
-  }catch (err){
-    next(err)
+  } catch (err) {
+    next(err);
   }
-})
+});
 
-
-
-
-
-module.exports = cartRouter
+module.exports = cartRouter;
--- src/server/db/cart.js
+++ src/server/db/cart.js
@@ -1,91 +1,91 @@
-const db = require("./client");
+const db = require('./client');
 
-// get cart 
+// get cart
 async function getCart(userId) {
-try{
-  const {rows} = await db.query(
-    `SELECT *
+  try {
+    const { rows } = await db.query(
+      `SELECT *
     FROM orders
-    WHERE user_id = $1 AND cart = true`,[userId]
-  );
+    WHERE user_id = $1 AND cart = true`,
+      [userId]
+    );
 
-  return rows [0];
-}catch (err){
-  throw err;
+    return rows[0];
+  } catch (err) {
+    throw err;
+  }
 }
-}
- 
-// getting cart items 
+
+// getting cart items
 async function getCartItems(cartId) {
-  try{
+  try {
     const { rows } = await db.query(
       `SELECT *
       FROM order_products 
-      WHERE order_id = $1`, [cartId]
+      WHERE order_id = $1`,
+      [cartId]
     );
     console.log(rows);
 
     return rows;
-  }catch (err){
+  } catch (err) {
     throw err;
   }
 }
 
 // past orders
 async function getPastOrders(userId) {
-  try{
-    const {rows} = await db.query(
+  try {
+    const { rows } = await db.query(
       `SELECT *
       FROM orders
-      WHERE user_id = $1 AND cart = false`,[userId]
+      WHERE user_id = $1 AND cart = false`,
+      [userId]
     );
-  
+
     return rows;
-  }catch (err){
-    throw err;
-  }
-  }
-
- // add item to cart 
-
- async function addToCart (order_id, product_id, quantity) {
-  try{
-    await db.query(
-      `INSERT INTO order_products(order_id, product_id, quantity)
-      VALUES ($1, $2, $3)`, [order_id, product_id, quantity]
-    );
-
-  }catch (err){
-    throw err;
-  }
- }
-
- // Update cart quantity
-async function updateCart (order_id, product_id, quantity){ 
-  try{
-    const {rows} = await db.query(
-      `UPDATE order_products SET quantity = $1 WHERE id = $2 RETURNING *`, [order_id, product_id, quantity]
-    );
-    return rows[0]
-  }catch (err){
+  } catch (err) {
     throw err;
   }
 }
 
+// add item to cart
 
+async function addToCart(order_id, product_id, quantity) {
+  try {
+    await db.query(
+      `INSERT INTO order_products(order_id, product_id, quantity)
+      VALUES ($1, $2, $3)`,
+      [order_id, product_id, quantity]
+    );
+  } catch (err) {
+    throw err;
+  }
+}
 
-
-
-
-
+// Update cart quantity
+async function updateCart(order_id, product_id, quantity) {
+  try {
+    const { rows } = await db.query(
+      `UPDATE order_products SET quantity = $1 WHERE id = $2 RETURNING *`,
+      [order_id, product_id, quantity]
+    );
+    return rows[0];
+  } catch (err) {
+    throw err;
+  }
+}
 
 // get users cart and past orders --- MAY NOT NEED ----
 async function getOrderItems(order_id) {
   try {
-    const { rows } = await db.query(` 
+    const { rows } = await db.query(
+      ` 
     SELECT *
     FROM order_products
-    WHERE order_id = $1 `, [order_id]);
+    WHERE order_id = $1 `,
+      [order_id]
+    );
 
     return rows[0];
   } catch (err) {
@@ -95,37 +95,33 @@ async function getOrderItems(order_id) {
 
 // get all orders from orders table
 async function getAllOrders() {
-  try{
-    const { rows } = await db.query("SELECT * FROM orders")
+  try {
+    const { rows } = await db.query('SELECT * FROM orders');
     return rows;
-  }catch (error){
-  throw error;
+  } catch (error) {
+    throw error;
   }
-};
+}
 
 // creates new orders
-async function createOrder (user_id, orderdate, shippingaddress, cart) {
-  try{
-    const {rows} = await db.query(
+async function createOrder(user_id, orderdate, shippingaddress, cart) {
+  try {
+    const { rows } = await db.query(
       `INSERT INTO orders (user_id, orderdate, shippingaddress, cart)
       VALUES ($1, $2, $3, $4)
       RETURNING *`,
       [user_id, orderdate, shippingaddress, cart]
     );
-    return rows [0];
-  }catch (error){
+    return rows[0];
+  } catch (error) {
     throw error;
   }
-
 }
 
 // creates the cart when you create a user -- does not need api endpoint
-async function createCart(user_id){
-  createOrder(user_id, null, null, true)
+async function createCart(user_id) {
+  createOrder(user_id, null, null, true);
 }
-
-
-
 
 module.exports = {
   getOrderItems,
@@ -136,5 +132,6 @@ module.exports = {
   getCartItems,
   getPastOrders,
   addToCart,
-  updateCart
+  updateCart,
+  deleteCartItem
 };
